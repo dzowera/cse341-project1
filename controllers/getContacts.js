9e1ac4d8daffe239e2acebc35bdd475901e5f49f
dzowera <idzowera@byupathway.edu>
2026-01-08 13:03:48 +0200

    get where it is boring!

---
+++ controllers/getContacts.js
@@ -0,0 +1,8 @@
+export const getContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching contacts" });
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
