1278c98acd089683691d31a538ed85dd296e971e
dzowera <idzowera@byupathway.edu>
2026-01-28 11:43:30 +0200

    updated the url

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://project1-0pl3.onrender.com",
         description: "Development server",
       },
     ],
